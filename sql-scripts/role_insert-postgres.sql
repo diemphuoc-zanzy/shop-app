203d965f6de7f5094854d6ccc856f785575405cf
quach.diem.phuoc <diemphuoc2626@gmail.com>
2024-12-09 17:01:35 +0700

    [feature]-update docker-compose

--- sql-scripts/role_insert-postgres.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO roles (name) VALUES ('ADMIN');
-INSERT INTO roles (name) VALUES ('SELLER');
-INSERT INTO roles (name) VALUES ('CUSTOMER');
-INSERT INTO roles (name) VALUES ('MODERATOR');
-INSERT INTO roles (name) VALUES ('GUEST');
