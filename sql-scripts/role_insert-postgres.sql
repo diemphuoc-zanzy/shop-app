dc48d62870263a6c51cbdf0d478fae65feda0cf1
quach.diem.phuoc <diemphuoc2626@gmail.com>
2024-12-09 07:09:09 +0700

    [feature]-add script database

---
+++ sql-scripts/role_insert-postgres.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (name) VALUES ('ADMIN');
+INSERT INTO roles (name) VALUES ('SELLER');
+INSERT INTO roles (name) VALUES ('CUSTOMER');
+INSERT INTO roles (name) VALUES ('MODERATOR');
+INSERT INTO roles (name) VALUES ('GUEST');
